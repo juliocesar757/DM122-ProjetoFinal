408a8f28d6d1d0b92a56686a78a79ef377f51dd4
Júlio César <49540652+juliocesar757@users.noreply.github.com>
2020-12-02 09:17:12 -0300

    Adicionado UfoService.js

---
+++ assets/js/UfoService.js
@@ -0,0 +1,45 @@
+import Dexie from 'https://cdn.jsdelivr.net/npm/dexie@3.0.3/dist/dexie.mjs';
+
+let db;
+
+export default class UfoService {
+    constructor() {
+        this.initializeDB();
+    }
+
+    initializeDB() {
+        db = new Dexie('ufoDB');
+
+        db.version(1).stores ({
+            transactions: '++id, timestamp'
+        });
+
+        db.on('populate', async () => {
+            await db.transactions.bulkPut([
+                { amount: 1000.00, description: "Salário", type: "inflow", timestamp: Date.now() + 1 },
+                { amount: 30.25, description: "Almoço fora", type: "outflow", timestamp: Date.now() + 2  },
+                { amount: 40.56, description: "Barzinho", type: "outflow", timestamp: Date.now() + 3  },
+                { amount: 50.85, description: "Cervejinha de sexta", type: "outflow", timestamp: Date.now() + 4  },
+                { amount: 100.00, description: "Achei na rua ;)", type: "inflow", timestamp: Date.now() + 5  },
+            ]);
+        });
+
+        db.open();
+    }
+
+    getAll() {
+        return db.transactions.toArray();
+    }
+
+    get(id) {
+        return db.transactions.get(id);
+    }
+
+    save(task) {
+        return db.transactions.put(task);
+    }
+
+    delete(id) {
+        return db.transactions.delete(id);
+    }
+}
